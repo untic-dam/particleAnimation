f1ffd5fc3051da554580ceae443ad087d1563d82
untic-dam <damien_modeling@outlook.fr>
2021-10-10 12:08:17 +0200

    Bounding function append

--- Particle.py
+++ Particle.py
@@ -3,17 +3,27 @@ class Particle():
     def __init__(self):
         self.x = 100
         self.y = 100
-        self.r = 10
-        self.ax = 1
-        self.ay = 1
+        self.r = 40
+        self.ax = 2
+        self.ay = -3
+        self.clr = color(12, 13, 100)
    
     def display(self): 
         strokeWeight(self.r)
-        stroke(0)
+        stroke(self.clr)
         point(self.x, self.y)
         
     def move(self):
         self.x += self.ax
         self.y += self.ay
+        self.handleEdge()
+        
+    def handleEdge(self):
+        if (self.x < 0) or (self.x > width) :
+            self.ax = -self.ax
+        if (self.y < 0) or (self.y > height) :
+            self.ay = -self.ay
+            
+        
     
     
